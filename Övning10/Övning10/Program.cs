ef956032006b486e82698a7ddda2308945a3be5b
Arvid Nilsson <arvid.nilsson2@elev.ga.ntig.se>
2024-02-11 20:31:28 +0100

    Final commit

---
+++ Övning10/Övning10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning10
+{
+    public class person
+    {
+        public string förnamn;
+        public string efternamn;
+        public int ålder;
+        public string intresse;
+
+        public string sammanfattning()
+        {
+            return $"Hej mitt namn är {förnamn} {efternamn}. Jag är {ålder} år gammal och gillar att {intresse}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person Person1 = new person();
+            Person1.förnamn = "Arvid";
+            Person1.efternamn = "Nilsson";
+            Person1.ålder = 18;
+            Person1.intresse = "vandra";
+
+            person Person2 = new person();
+            Person2.förnamn = "Daniel";
+            Person2.efternamn = "Nilsson";
+            Person2.ålder = 49;
+            Person2.intresse = "åka skidor";
+
+            Console.WriteLine(Person1.sammanfattning());
+            Console.WriteLine(Person2.sammanfattning());
+
+        }
+    }
+}
---
+++ Övning11/Övning11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning11
+{
+    public class Bil
+    {
+        public double TankStorlek;
+        public double KvarvarandeBränsle;
+        public double Bränsleförbrukning;
+
+        public double HurMycketKanManTanka()
+        {
+            return TankStorlek - KvarvarandeBränsle;
+        }
+        public double Räckvidd()
+        {
+            return KvarvarandeBränsle * Bränsleförbrukning;
+        }
+        public string Sammanfattning()
+        {
+            return $"Bilen har {KvarvarandeBränsle} Liter kvar i tanken. Om du vill tanka kan du max tanka {HurMycketKanManTanka()} Liter, annars kan du köra {Räckvidd()} km till tanken är tom.";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating object car
+            Bil bil = new Bil();
+            bil.TankStorlek = 100;
+            bil.KvarvarandeBränsle = 42;
+            bil.Bränsleförbrukning = 2;
+            //Writing summary for car
+            Console.WriteLine(bil.Sammanfattning());    
+        }
+    }
+}
